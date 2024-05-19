9588a5e37b211dc0b5ea3b733e2efc239acdfc7d
therealalexolthoff <alex@1452design.com>
2024-05-19 01:15:23 -0400

    Complete the set up and render of simple scene
    Committing to record completion of initial set up

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 import * as THREE from 'three'
 
+// Non-three.js variable set up
+const canvasEl = document.querySelector('.webgl')
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Three.js variable set up
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera()
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry,material)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvasEl
+})
+
+// Adjust size and positions
+renderer.setSize(sizes.width, sizes.height)
+camera.position.z = 3
+
+// Add content to scene
 scene.add(mesh)
+scene.add(camera)
+
+// Render Scene
+renderer.render(scene, camera)
