054cbd869bf83fcc5a7ddd3c22b007065ab3ea52
therealalexolthoff <alex@1452design.com>
2024-05-19 01:08:14 -0400

    Set up basic Three JS scene with red cube

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 import * as THREE from 'three'
-console.log('JavaScript is working')
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera()
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry,material)
+scene.add(mesh)
