ab9edc70fc7b809f359eb19ce47a2770acab2518
therealalexolthoff <alex@1452design.com>
2024-05-21 09:12:01 -0400

    Added a few key methods for my own reference

--- script.js
+++ script.js
@@ -13,13 +13,22 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry,material)
+mesh.position.y = 1
 const renderer = new THREE.WebGLRenderer({
     canvas: canvasEl
 })
 
+
+
 // Adjust size and positions
 renderer.setSize(sizes.width, sizes.height)
 camera.position.z = 3
+
+// Console logging a few important methods
+console.log(mesh.position.length())
+console.log(mesh.position.distanceTo(camera.position))
+console.log(mesh.position.normalize())
+mesh.position.set(0.7, -0.6, 1)
 
 // Add content to scene
 scene.add(mesh)
