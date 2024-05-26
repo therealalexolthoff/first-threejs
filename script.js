2835f49f1a58a0ae7948abdae80638f14ab20d77
therealalexolthoff <alex@1452design.com>
2024-05-26 15:19:55 -0400

    Make the canvas full screen

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+
+// import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 // Non-three.js  set up
 
 const canvasEl = document.querySelector('.webgl')
-const cursor = {
-    x: 0,
-    y: 0
-}
+// const cursor = {
+//     x: 0,
+//     y: 0
+// }
 
-window.addEventListener('mousemove', (e) => {
-    cursor.x = e.clientX / sizes.width - 0.5
-    cursor.y = - (e.clientY / sizes.height - 0.5)
-    console.log(cursor)
-})
+// window.addEventListener('mousemove', (e) => {
+//     cursor.x = e.clientX / sizes.width - 0.5
+//     cursor.y = - (e.clientY / sizes.height - 0.5)
+//     console.log(cursor)
+// })
 
+
+// Different ways to set up the size of the scene (canvas)
+
+// Static
+// const sizes = {
+//     width: 800,
+//     height: 600
+// }
+// Full screen (once you get rid of margins)
 const sizes = {
-    width: window.visualViewport.width,
-    height: window.visualViewport.height
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+// const sizes = {
+//     width: window.visualViewport.width,
+//     height: window.visualViewport.height
+// }
 
 
 
@@ -99,8 +115,9 @@ group.scale.x = .4
 group.scale.y = .4
 // group.rotation.x = 2.5
 
-
-
+// Setting up controls
+const controls = new OrbitControls(camera,canvasEl)
+controls.enableDamping = true
 // Setting the lookat
 // camera.lookAt(mesh.position)
 
@@ -146,9 +163,17 @@ const tick = () => {
     // gsap.to(group.position, { duration: 1, delay: 1, x: -1 })
 
     // Cursor movement moves camera
-    camera.position.x = cursor.x * 3
-    camera.position.y = cursor.y * 3
-    camera.lookAt(group.position)
+    // Note: Changing the value the sin and cos acts on will increase the rate at which the object turns. Making this number 4 will result in one full rotation in either direction
+    // Changing the number outside the sin and cos will increase the distance from the camera and thus improve the ability to visualize the rotation
+    // Setting a y here will make sure that you can see the top and bottom. Also, removing the position update on z will get cause the rotation to lack a realistic turn
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 4) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 4) * 3
+    // camera.position.y = cursor.y * 2
+    // camera.lookAt(group.position)
+
+    // Update Controls
+    controls.update()
+
     // Render Scene
     renderer.render(scene, camera)
 
